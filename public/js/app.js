25388fee8e3fef11f240f19e23db4c05539460db
Sandra YOUSSOUF <151831795+sandraYoussouf@users.noreply.github.com>
2024-04-02 18:39:36 +0200

    Add Javascript
    - Ajout du javascript faisant fonctionner la galerie d'un article

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+// Page produit : gallerie d'images
+
+const yield = document.querySelector('.img-container > .product-img');
+const imgGallery = document.querySelectorAll('.img-gallery');
+
+console.log(yield)
+
+const pushImage = (image) => {
+
+  imgGallery.forEach(img => {
+    img.classList.add("inactive");
+  });
+
+  image.classList.remove("inactive");
+
+  yield.src = image.src;
+}
+
+// Fin Page produit : gallerie d'images
