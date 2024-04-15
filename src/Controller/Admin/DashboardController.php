213c78a7ecf0f976b686bd93535dae1e75a7cbd4
Sandra <sandra.youssoufh@gmail.com>
2024-04-15 21:21:43 +0200

    Initial commit

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Entity\Voiture;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+// use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController; test lien register
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        // return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Garage');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'home');
+        yield MenuItem::linkToCrud('Employés', 'fas fa-user', User::class);
+        // yield MenuItem::linkToCrud('Ajouter un employé', 'fas fa-plus', 'app_register'); test lien register
+        yield MenuItem::linkToCrud('Voitures', 'fas fa-car', Voiture::class);
+    }
+}
---
+++ src/Controller/NosVehiculesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Voiture; //Pour l'affichage de l'entité voiture en front
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface; //Pour l'affichage de l'entité voiture en front
+
+class NosVehiculesController extends AbstractController
+{
+    //Pour l'affichage de l'entité voiture en front
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/vehicules', name: 'vehicules')]
+    public function index(): Response
+    {
+        //Pour l'affichage de l'entité voiture en front
+        $repository = $this->entityManager->getRepository(Voiture::class);
+        $voitures = $repository->findAll();
+
+        return $this->render('nos_vehicules/index.html.twig', [
+            'voiture' => $voitures, //Pour l'affichage de l'entité voiture en front
+            'controller_name' => 'NosVehiculesController',
+        ]);
+    }
+}
+
+// Aide de Caro
+
+    // use Symfony\Component\HttpFoundation\Request; //Etape 1 : Request pour l'affichage de l'entité voiture en front
+    // use Symfony\Component\HttpFoundation\Session\SessionInterface; // Etape 1 : pour l'affichage de l'entité voiture en front
+    
+    // public function index(Request $req, SessionInterface $session): Response #Ca je sais pas trop :')
+    // {
+    //     $repository = $this->entityManager->getRepository(Voiture::class); #Du coup ici on a une variable reporitsory qui va récupérer la table Voiture
+    //     $voitures = $repository->findAll(); #On créer une variable voiture et dedans on va sélécyionner toutes les voitures
+
+    //     return $this->render('home/index.html.twig', [
+    //         'voitures' => $voitures, #Puis pour ton twig, tout ce qui est égal à 'voiture' sera du coup la variable voiture
+    //     ]);
+    // }
